8f69d0e10b590825cf43ecb509ab13c3e21e9d92
Muhammed Yasir Akyüz <muhammedyasir.akyuz@gmail.com>
2025-02-07 20:36:00 +0100

    BaseEntity Konfigürasyonları eklendi. Her bir base entity için ayrı Konfigürasyon classları oluşturuldu.

---
+++ MyBlogProject.Domain/Core/BaseEntityConfigurations/AuditableEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBlogProject.Domain.Core.BaseEntities;
+
+namespace MyBlogProject.Domain.Core.BaseEntityConfigurations;
+
+public class AuditableEntityConfiguration<TEntity> : BaseEntityConfiguration<TEntity> where TEntity : AuditableEntity
+{
+    public override void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        base.Configure(builder);
+        builder.Property(x => x.DeletedDate).IsRequired(false);
+        builder.Property(x => x.DeletedBy).IsRequired(false);
+    }
+}
---
+++ MyBlogProject.Domain/Core/BaseEntityConfigurations/BaseUserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBlogProject.Domain.Core.BaseEntities;
+
+namespace MyBlogProject.Domain.Core.BaseEntityConfigurations;
+
+public class BaseUserConfiguration<TEntity>: AuditableEntityConfiguration<TEntity> where TEntity: BaseUser
+{
+    public override void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        base.Configure(builder);
+        builder.Property(u => u.FirstName).IsRequired().HasMaxLength(128);
+        builder.Property(u=>u.LastName).IsRequired().HasMaxLength(128);
+        builder.Property(u => u.Email).IsRequired();
+    }
+}
